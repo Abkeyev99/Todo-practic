838c441c09e2fcf10a7b4233e30ad2417850c907
Arhci <arlan.abkeyev@mail.ru>
2023-10-30 04:07:29 +0300

    refacter new function handler

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -48,13 +48,16 @@ export function Todolist(props: PropsType) {
         </div>
         <ul>
             {
-                props.tasks.map(t => <li key={t.id}>
+                props.tasks.map(t =>{
+
+                    const onRemoveHandler = () => {  props.removeTask(t.id)}
+
+                    return <li key={t.id}>
                     <input type="checkbox" checked={t.isDone}/> <span>{t.title}</span>
-                    <button onClick={() => {
-                        props.removeTask(t.id)
-                    }}>x
-                    </button>
-                </li>)}
+                    <button onClick={onRemoveHandler}>x</button>
+                </li>
+                })
+            }
         </ul>
         <div>
             <button onClick={onAllClickHandler}>All
